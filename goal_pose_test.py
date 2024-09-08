bbab4836f1380c2298f99a08e10bdf481aee7162
Vinay Kumar Gupta <108336700+Vinay-Kumar-Gupta@users.noreply.github.com>
2024-09-08 15:49:46 +0530

    Update goal_pose_test.py

--- goal_pose_test.py
+++ goal_pose_test.py
@@ -1,5 +1,6 @@
-# This is a Publisher Module for publishing goal_pose 
-# This Can be imported and used as a function directly by passing the argument
+# This function will be used for publishing various topics for controlling the robot
+# you can directly import this module and use the functions
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped
@@ -10,14 +11,17 @@ class GoalPosePublisher(Node):
         super().__init__('goal_pose_publisher')
         self.publisher_ = self.create_publisher(PoseStamped, '/goal_pose', 10)
 
-        # Dictionary storing room names and their coordinates (x, y)
-        self.room_coordinates = {
-            'room305': (8.0, -0.7),
-            'room311': (0.6, -4.8),
-            'room303': (5.1, 6.3)
-        }
 
-    def post_goal(self, room_name):
+        # Dictionary storing room names and their coordinates (x, y)
+
+        self.room_coordinates = { 'room_305': (4.9, 3.6),    'room_306': (12.8, 3.38),    'room_307': (18.5, 4.3), 
+                                  'room_308': (27.0, 3.1),   'room_309': (32.9, 3.6),     'room_310': (39.25, 3.61), 
+                                  'room_311': (38.8, -5.9),  'room_312': (32.49, -6.72),  'room_313': (25.41, -5.81), 
+                                  'room_314': (19.91,-6.03), 'room_315': (10.96, -7.09),  'room_316': (6.03,-6.55) }
+        
+ # ================================== GOAL POSE PUBLISHER ==================================
+
+    def publish_goal_pose(self, room_name):
         if room_name in self.room_coordinates:
             x, y = self.room_coordinates[room_name]
 
@@ -44,7 +48,16 @@ class GoalPosePublisher(Node):
             # rclpy.shutdown()  # Exit the code after publishing
         else:
             self.get_logger().error(f'Room name {room_name} not found!')
+    
+    def publish_cmd_vel():
+        pass
 
+    def Tour_Mode():
+        pass
+
+    def Emergency_Mode():
+        pass
+    
 # def main(args=None):
 #     rclpy.init(args=args)
 #     node = GoalPosePublisher()
